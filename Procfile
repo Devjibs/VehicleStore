b9175400d3b36b325689064e7bf560dcc91ac42a
JiboGithub <ajidejibola@yahoo.com>
2021-07-21 23:49:23 +0100

    final deployment

---
+++ Procfile
@@ -0,0 +1,3 @@
+
+
+web gunicorn googleclone.wsgi:application --log-file -
---
+++ pages/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('services', views.service, name='services'),
+    path('contact', views.contact, name='contact'),
+]
